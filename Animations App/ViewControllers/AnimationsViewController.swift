b9b9be380ebf472af69ce284b4d4d51fc10f482b
denkitdam <cryptoden145@gmail.com>
2023-05-03 21:48:15 +0300

    primary animations logic setup

--- Animations App/ViewControllers/AnimationsViewController.swift
+++ Animations App/ViewControllers/AnimationsViewController.swift
@@ -10,7 +10,7 @@ import SpringAnimation
 
 class AnimationsViewController: UIViewController {
     
-    @IBOutlet var AnimationsView: SpringView!
+    @IBOutlet var animationsView: SpringView!
     
     @IBOutlet var presetLabel: UILabel!
     @IBOutlet var curveLabel: UILabel!
@@ -18,14 +18,35 @@ class AnimationsViewController: UIViewController {
     @IBOutlet var durationLabel: UILabel!
     @IBOutlet var delayLabel: UILabel!
 
+    private let animations = DataStore.shared.getAnimations()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        animationsView.layer.cornerRadius = 15
        
     }
 
     @IBAction func animationsButtonTapped() {
+        let animation = animations.randomElement()
+        animationsView.animation = animation?.preset ?? ""
+        animationsView.curve = animation?.curve ?? ""
+        animationsView.force = animation?.force ?? 0
+        animationsView.duration = animation?.duration ?? 0
+        animationsView.delay = animation?.delay ?? 0
         
+        setupLabelsFor(animation: animation)
+        
+        animationsView.animate()
+        
+    }
+    
+    private func setupLabelsFor(animation: Animation?) {
+        presetLabel.text = " Preset: \(animation?.preset ?? "")"
+        curveLabel.text = "Curve \(animation?.curve ?? "")"
+        forceLabel.text = "Force \(animation?.force.formatted() ?? "")"
+        durationLabel.text = "Duration \(animation?.duration.formatted() ?? "")"
+        delayLabel.text = "Delay \(animation?.delay.formatted() ?? "")"
     }
 }
 
